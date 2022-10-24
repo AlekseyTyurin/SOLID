60221ef20f87f14804707f136c59ee1917f43049
Alexey Tyurin <alexgeolog@gmail.com>
2022-10-24 21:02:31 +0300

    compatible Single Responsive principle

--- 1_S.js
+++ 1_S.js
@@ -1,5 +1,6 @@
 // Single Responsibility Principle
 
+// This class responsible only for create and update news
 class News {
     constructor(title, text) {
         this.title = title
@@ -8,29 +9,50 @@ class News {
     }
 
 
-    update(text) {
+    updateNews(text) {
         this.text = text
         this.modified = true
     }
+}
 
-    toHTML() {
+// This class responsible only for format news and can be extended
+class NewsFormatter {
+    constructor(news) {
+        this.news = news
+    }
+
+    html() {
         return `
             <div class='news'>
-                <h1>${this.title}</h1>
-                <p>${this.text}</p>
+                <h1>${this.news.title}</h1>
+                <p>${this.news.text}</p>
             </div>
         `
     }
 
-    toJSON() {
+    json() {
         return JSON.stringify({
-            title: this.title,
-            text: this.text,
-            modified: this.modified
+            title: this.news.title,
+            text: this.news.text,
+            modified: this.news.modified
         }, null, 2)
+    }
+
+    //can add another method for formatting
+    xml() {
+        return `
+            <news>
+                <title>${this.news.title}</title>
+                <text>${this.news.text}</text>
+            </news>
+        `
     }
 }
 
-const news = new News('Ukraine forces', 'Win the war with russia');
+const formatter = new NewsFormatter(new News('Ukraine forces', 'Win the war with russia'))
+// const news = new News('Ukraine forces', 'Win the war with russia');
 
-console.log(news.toJSON())
+// console.log(news.toJSON())
+console.log(formatter.html())
+console.log(formatter.json())
+console.log(formatter.xml())
